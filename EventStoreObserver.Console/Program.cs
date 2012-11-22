f554caf33f94c0afc7cd0c4c1ab8c7682486dcf4
Anton Vynogradenko <restuta8@gmail.com>
2012-11-22 01:38:21 -0800

    added plan for the very first version

--- EventStoreObserver.Console/Program.cs
+++ EventStoreObserver.Console/Program.cs
@@ -10,6 +10,14 @@ namespace EventStoreObserver.Console
     {
         static void Main(string[] args)
         {
+            //v1
+            //connect to mongo
+            //start quering EventStore collection (assuming it's one for now?) with time intervals, let's say once in
+            //      half-second, investigate any mongo notifiers, hooks
+            //      investigate if it's possible to hook into event writing process right into app or event store layer
+            //
+            //format message
+            //output colored
         }
     }
 }
