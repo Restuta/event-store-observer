4c88b7f5ba4951ddd26adad5b9684aabcdeb1c7f
Anton Vynogradenko <restuta8@gmail.com>
2012-11-25 20:47:17 -0800

    added ColorfulConsole and several string extensions

--- EventStoreObserver.Console/Program.cs
+++ EventStoreObserver.Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Restuta.ConsoleExtensions.Colorfull;
 
 namespace EventStoreObserver.Console
 {
@@ -10,6 +11,8 @@ namespace EventStoreObserver.Console
     {
         static void Main(string[] args)
         {
+
+            
             //v1
             //connect to mongo
             //start quering EventStore collection (assuming it's one for now?) with time intervals, let's say once in
@@ -18,6 +21,16 @@ namespace EventStoreObserver.Console
             //
             //format message
             //output colored
+        }
+
+        public static void W(string text)
+        {
+            ColorfulConsole.Write(text);
+        }
+
+        public static void WL(string text)
+        {
+            ColorfulConsole.WriteLine(text);
         }
     }
 }
---
+++ EventStoreObserver.Console/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+using Restuta.ConsoleExtensions.Colorfull;
+
+namespace EventStoreObserver.Console
+{
+    public static class StringExtensions
+    {
+        [StringFormatMethod("source")]
+        public static string F(this string source, params object[] args)
+        {
+            return string.Format(source, args);
+        }
+
+        [StringFormatMethod("source")]
+        public static string F(this ColorfulString source, params object[] args)
+        {
+            return string.Format(source, args);
+        }
+    }
+}
